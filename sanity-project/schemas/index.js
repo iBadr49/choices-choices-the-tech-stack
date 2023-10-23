ecb7facbf6dd373b480f406545962c7453a9ec81
iBadr49 <badr.amara@hva.nl>
2023-10-23 19:44:22 +0200

    ...

--- sanity-project/schemas/index.js
+++ sanity-project/schemas/index.js
@@ -1,3 +1,5 @@
 // schemas/index.js
-import pet from './pet'
-export const schemaTypes = [pet]
+import pet from './pet';
+import stekje from './stekjes'; // Importeren van het stekje-schema dat ik heb gemaakt
+export const schemaTypes = [pet, stekje]; 
+
---
+++ sanity-project/schemas/stekjes.js
@@ -0,0 +1,23 @@
+// schemas/stekje.js
+export default {
+  name: 'stekjes',
+  type: 'document',
+  title: 'Stekjes',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Name'
+    },
+    {
+      name: 'image',
+      type: 'image',
+      title: 'Image',
+      options: {
+        hotspot: true, // Hiermee kun je het focuspunt voor de afbeelding instellen
+      }
+    }
+  ]
+};
+
+  
